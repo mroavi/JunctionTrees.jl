6caaead98b40ca4bd9e3805c7824559a4d965f52
Martin Roa Villescas <mroavi@gmail.com>
2021-03-04 08:45:21 +0100

    Adapt tests for new factor implementations

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,29 +6,29 @@ using Test
   # Factor product
 
   A = Factor{Float64,1}((1,), [0.11; 0.89])
-  B = Factor{Float64,2}((2, 1), [0.59 0.22; 0.41 0.78])
+  B = Factor{Float64,2}((1, 2), [0.59 0.41; 0.22 0.78])
   C = product(A, B)
   @test C.vars == (1, 2)
   @test C.vals ≈ [0.0649 0.0451; 0.1958 0.6942]
 
-  A = Factor{Float64,2}((3, 2), [0.5 0.1; 0.7 0.2])
-  B = Factor{Float64,2}((2, 1), [0.5 0.1 0.3; 0.8 0.0 0.9])
+  A = Factor{Float64,2}((2, 3), [0.5 0.7; 0.1 0.2])
+  B = Factor{Float64,2}((1, 2), [0.5 0.8; 0.1 0.0; 0.3 0.9])
   C = product(A, B)
-  @test C.vars == (3, 2, 1)
-  @test vec(C.vals) ≈ [0.2500, 0.3500, 0.0800, 0.1600, 0.0500, 0.0700,
-                       0.0000, 0.0000, 0.1500, 0.2100, 0.0900, 0.1800]
+  @test C.vars == (1, 2, 3)
+  @test C.vals ≈ cat([0.25 0.08; 0.05 0.0; 0.15 0.09],
+                     [0.35 0.16; 0.07 0.0; 0.21 0.18], dims=3)
 
-  A = Factor{Float64,2}((2, 1), [0.5 0.1 0.3; 0.8 0.0 0.9])
+  A = Factor{Float64,2}((1, 2), [0.5 0.8; 0.1 0.0; 0.3 0.9])
   B = Factor{Float64,0}((), Array{Float64,0}(undef))
   C = product(A, B)
-  @test C.vars == (2, 1)
-  @test C.vals ≈ [0.5 0.1 0.3; 0.8 0.0 0.9]
+  @test C.vars == (1, 2)
+  @test C.vals ≈ [0.5 0.8; 0.1 0.0; 0.3 0.9]
 
   A = Factor{Float64,0}((), Array{Float64,0}(undef))
-  B = Factor{Float64,2}((2, 1), [0.5 0.1 0.3; 0.8 0.0 0.9])
+  B = Factor{Float64,2}((1, 2), [0.5 0.8; 0.1 0.0; 0.3 0.9])
   C = product(A, B)
-  @test C.vars == (2, 1)
-  @test C.vals ≈ [0.5 0.1 0.3; 0.8 0.0 0.9]
+  @test C.vars == (1, 2)
+  @test C.vals ≈ [0.5 0.8; 0.1 0.0; 0.3 0.9]
 
   A = Factor{Float64,0}((), Array{Float64,0}(undef))
   B = Factor{Float64,0}((), Array{Float64,0}(undef))
@@ -37,37 +37,37 @@ using Test
   @test ndims(C.vals) == 0
 
   A = Factor{Float64,1}((1,), [0.11; 0.89])
-  B = Factor{Float64,2}((2, 1), [0.59 0.22; 0.41 0.78])
+  B = Factor{Float64,2}((1, 2), [0.59 0.41; 0.22 0.78])
   C = product([A, B])
   @test C.vars == (1, 2)
   @test C.vals ≈ [0.0649 0.0451; 0.1958 0.6942]
 
   # Factor marginalization
 
-  A = Factor{Float64,2}((2, 1), [0.59 0.41; 0.22 0.78])
-  V = [1]
-  B = marg(A, V)
-  @test B.vars == (2,)
-  @test B.vals ≈ [1.0, 1.0]
-
-  A = Factor{Float64,3}((3, 2, 1), cat([0.25 0.08; 0.35 0.16],
-                            [0.05 0.00; 0.07 0.00], 
-                            [0.15 0.09; 0.21 0.18], dims=3))
+  A = Factor{Float64,2}((1, 2), [0.59 0.41; 0.22 0.78])
   V = [2]
   B = marg(A, V)
-  @test B.vars == (3, 1)
-  @test vec(B.vals) ≈ [0.33, 0.51, 0.05, 0.07, 0.24, 0.39]
+  @test B.vars == (1,)
+  @test B.vals ≈ [1.0, 1.0]
+
+  A = Factor{Float64,3}((1, 2, 3), cat([0.25 0.08; 0.05 0.0; 0.15 0.09],
+                                       [0.35 0.16; 0.07 0.0; 0.21 0.18], dims=3))
+
+
+  V = [2]
+  B = marg(A, V)
+  @test B.vars == (1, 3)
+  @test vec(B.vals) ≈ [0.33, 0.05, 0.24, 0.51, 0.07, 0.39]
 
   # Factor reduction
 
-  A = Factor{Float64,3}((3, 2, 1), cat([0.25 0.08; 0.35 0.16],
-                            [0.05 0.00; 0.07 0.00], 
-                            [0.15 0.09; 0.21 0.18], dims=3))
+  A = Factor{Float64,3}((1, 2, 3), cat([0.25 0.08; 0.05 0.0; 0.15 0.09],
+                                       [0.35 0.16; 0.07 0.0; 0.21 0.18], dims=3))
   var = 3
   val = 1
   B = redu(A, var, val)
-  @test B.vars == (2, 1)
-  @test vec(B.vals) ≈ [0.25, 0.08, 0.05, 0.00, 0.15, 0.09]
+  @test B.vars == (1, 2)
+  @test B.vals ≈ [0.25 0.08; 0.05 0.0; 0.15 0.09]
 
   # Factor normalization
 
@@ -75,13 +75,12 @@ using Test
   C = norm(A)
   @test sum(C.vals) ≈ 1.0
 
-  A = Factor{Float64,2}((2, 1), [0.59 0.22; 0.41 0.78])
+  B = Factor{Float64,2}((1, 2), [0.59 0.41; 0.22 0.78])
   C = norm(A)
   @test sum(C.vals) ≈ 1.0
 
-  A = Factor{Float64,3}((3, 2, 1), cat([0.25 0.08; 0.35 0.16],
-                            [0.05 0.00; 0.07 0.00], 
-                            [0.15 0.09; 0.21 0.18], dims=3))
+  A = Factor{Float64,3}((1, 2, 3), cat([0.25 0.08; 0.05 0.0; 0.15 0.09],
+                                       [0.35 0.16; 0.07 0.0; 0.21 0.18], dims=3))
   C = norm(A)
   @test sum(C.vals) ≈ 1.0
 
