4a7e305b69c65fdc3b357dea20ab19429c91ed57
GiggleLiu <cacate0129@gmail.com>
2022-06-08 01:57:58 -0400

    back-propagation

--- src/omeinsum.jl
+++ src/omeinsum.jl
@@ -11,6 +11,9 @@ end
 function TensorNetworksSolver(factors::Vector{<:Factor}; openvertices=(), fixedvertices=Dict{Int,Int}(), optimizer=GreedyMethod(), simplifier=nothing)
     tensors = getfield.(factors, :vals)
     rawcode = EinCode([[factor.vars...] for factor in factors], collect(Int, openvertices))  # labels for edge tensors
+    TensorNetworksSolver(rawcode, tensors; fixedvertices, optimizer, simplifier)
+end
+function TensorNetworksSolver(rawcode::EinCode, tensors::Vector{<:AbstractArray}; fixedvertices=Dict{Int,Int}(), optimizer=GreedyMethod(), simplifier=nothing)
     code = optimize_code(rawcode, OMEinsum.get_size_dict(getixsv(rawcode), tensors), optimizer, simplifier)
     TensorNetworksSolver(code, tensors, fixedvertices)
 end
@@ -63,3 +66,69 @@ end
 popindices!(se::SlicedEinsum, indices, out) = popindices!(se.eins, indices, out)
 popindices(code, indices) = (code = deepcopy(code); popindices!(code, indices, []); code)
 
+# METHOD3: back propagation
+# the data structure storing intermediate `NestedEinsum` contraction results.
+struct CacheTree{T}
+    content::AbstractArray{T}
+    siblings::Vector{CacheTree{T}}
+end
+function cached_einsum(se::SlicedEinsum, @nospecialize(xs), size_dict)
+    if length(se.slicing) != 0
+        @warn "Slicing is not supported for caching, got nslices = $(length(se.slicing))! Fallback to `NestedEinsum`."
+    end
+    return cached_einsum(se.eins, xs, size_dict)
+end
+function cached_einsum(code::NestedEinsum, @nospecialize(xs), size_dict)
+    if OMEinsum.isleaf(code)
+        y = xs[code.tensorindex]
+        return CacheTree(y, CacheTree{eltype(y)}[])
+    else
+        caches = [cached_einsum(arg, xs, size_dict) for arg in code.args]
+        y = einsum(code.eins, ntuple(i->caches[i].content, length(caches)), size_dict)
+        return CacheTree(y, caches)
+    end
+end
+
+# computed gradient tree by back propagation
+function generate_gradient_tree(se::SlicedEinsum, cache::CacheTree{T}, dy::AbstractArray{T}, size_dict::Dict) where T
+    if length(se.slicing) != 0
+        @warn "Slicing is not supported for generating masked tree! Fallback to `NestedEinsum`."
+    end
+    return generate_gradient_tree(se.eins, cache, dy, size_dict)
+end
+function generate_gradient_tree(code::NestedEinsum, cache::CacheTree{T}, dy::AbstractArray{T}, size_dict::Dict) where T
+    if OMEinsum.isleaf(code)
+        return CacheTree(dy, CacheTree{T}[])
+    else
+        xs = (getfield.(cache.siblings, :content)...,)
+        dxs = ntuple(i -> OMEinsum.einsum_grad(OMEinsum.getixs(code.eins), xs, OMEinsum.getiy(code.eins), size_dict, conj(dy), i), length(xs))
+        return CacheTree(dy, generate_gradient_tree.(code.args, cache.siblings, dxs, Ref(size_dict)))
+    end
+end
+
+function gradient_tree(code, xs)
+    size_dict = OMEinsum.get_size_dict!(getixsv(code), xs, Dict{Int,Int}())
+    cache = cached_einsum(code, xs, size_dict)
+    dy = fill!(similar(cache.content), one(eltype(cache.content)))
+    return generate_gradient_tree(code, cache, dy, size_dict)
+end
+
+function gradient(code, xs)
+    tree = gradient_tree(code, xs)
+    return extract_leaves(code, tree)
+end
+
+extract_leaves(code::SlicedEinsum, cache::CacheTree) = extract_leaves(code.eins, cache)
+function extract_leaves(code::NestedEinsum, cache::CacheTree)
+    res = Vector{Any}(undef, length(getixsv(code)))
+    return extract_leaves!(code, cache, res)
+end
+
+function extract_leaves!(code, cache, res)
+    if OMEinsum.isleaf(code)
+        res[code.tensorindex] = cache.content
+    else
+        extract_leaves!.(code.args, cache.siblings, Ref(res))
+    end
+    return res
+end
--- test/omeinsum.jl
+++ test/omeinsum.jl
@@ -60,7 +60,7 @@ end
 
     reference_marginals = JunctionTrees.read_uai_mar_file(uai_mar_filepath)
     obsvars, obsvals = JunctionTrees.read_uai_evid_file(uai_evid_filepath)
-    nvars, cards, nclique, factors = JunctionTrees.read_uai_file(uai_filepath)
+    nvars, cards, nclique, factors = JunctionTrees.read_uai_file(uai_filepath; factor_eltype=Float32)
 
     # does not optimize over open vertices
     tn = TensorNetworksSolver(factors; fixedvertices=Dict(zip(obsvars, obsvals .- 1)), openvertices=Int[], optimizer=TreeSA(ntrials=1))
@@ -73,8 +73,9 @@ end
     # for dangling vertices, the output size is 1.
     npass = 0
     for i=1:nvars
-        npass += (length(marginals[i]) == 1 && reference_marginals[i] == [0.0, 1]) || isapprox(marginals[i], reference_marginals[i]; rtol=1e-1, atol=1e-4)
+        npass += (length(marginals[i]) == 1 && reference_marginals[i] == [0.0, 1]) || isapprox(marginals[i], reference_marginals[i])
     end
+    @show npass, nvars
     @test npass >= nvars / 2
 
     # an alternative approach, commented out because it is slower in tests.
@@ -92,4 +93,35 @@ end
             )
     eval(algo)
     @time marginals = run_algo(obsvars, obsvals) |> x -> map(y -> y.vals, x)
+end 
+
+@testset "tensor network solvers" begin
+    ################# Load problem ####################
+    problem_number = "14"
+    problem_filename = joinpath("Promedus_" * problem_number)
+    problem_dir = joinpath(dirname(@__DIR__), "examples/problems/Promedus/", problem_number)
+    uai_filepath = joinpath(problem_dir, problem_filename * ".uai")
+    uai_evid_filepath = joinpath(problem_dir, problem_filename * ".uai.evid")
+    uai_mar_filepath = joinpath(problem_dir, problem_filename * ".uai.MAR")
+    td_filepath = joinpath(problem_dir, problem_filename * ".td")
+
+    reference_marginals = JunctionTrees.read_uai_mar_file(uai_mar_filepath)
+    obsvars, obsvals = JunctionTrees.read_uai_evid_file(uai_evid_filepath)
+    nvars, cards, nclique, factors = JunctionTrees.read_uai_file(uai_filepath; factor_eltype=Float32)
+
+    # does not optimize over open vertices
+    rawcode = EinCode([[[i] for i in 1:nvars]..., [[factor.vars...] for factor in factors]...], Int[])  # labels for edge tensors
+    @show rawcode
+    tensors = [[ones(Float32, 2) for i=1:length(cards)]..., getfield.(factors, :vals)...]
+    tn = TensorNetworksSolver(rawcode, tensors; fixedvertices=Dict(zip(obsvars, obsvals .- 1)), optimizer=TreeSA(ntrials=1))
+    @info timespace_complexity(tn.code, OMEinsum.get_size_dict(getixsv(tn.code), tensors))
+    @time marginals2 = JunctionTrees.gradient(tn.code, generate_tensors(tn))[1:length(cards)] .|> LinearAlgebra.normalize!
+    @show marginals2
+    # for dangling vertices, the output size is 1.
+    npass = 0
+    for i=1:nvars
+        npass += (length(marginals2[i]) == 1 && reference_marginals[i] == [0.0, 1]) || isapprox(marginals2[i], reference_marginals[i])
+    end
+    @show npass, nvars
+    @test npass >= nvars / 2
 end 
