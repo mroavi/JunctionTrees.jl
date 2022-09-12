75fc5033588973d651f077b875d7a34ba79e9868
Martin Roa Villescas <mroavi@gmail.com>
2022-09-12 20:24:03 +0200

    Remove time measurements for testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,7 @@ for file in filter(f -> endswith(f, ".jl"), readdir(@__DIR__))
   end
 
   @testset "$(file)" begin
-    t = time()
     include(file)
-    println("$(file) took $(round(time() - t; digits = 1)) seconds.")
   end
 
 end
