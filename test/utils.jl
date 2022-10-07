b14fec3b6e3645d9651690a1f97e9cc929b94cff
Martin Roa Villescas <mroavi@gmail.com>
2022-10-07 21:33:17 +0200

    Fix uai2014 artifact installation in CI

--- test/utils.jl
+++ test/utils.jl
@@ -6,9 +6,6 @@ using Artifacts
 
   @testset "Tree decompositions" begin
 
-    import Pkg
-    Pkg.ensure_artifact_installed("uai2014", "Artifacts.toml")
-
     td_filepath = joinpath(artifact"uai2014", "Promedus_34.tamaki.td")
     @test get_td_soln(td_filepath) == [353, 15, 415]
 
