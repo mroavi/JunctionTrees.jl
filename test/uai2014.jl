79af1e035c7d1987bdd2bcc34f212c1b985e540e
Martin Roa Villescas <mroavi@gmail.com>
2022-10-11 15:38:10 +0200

    Update uai2014 tests

--- test/uai2014.jl
+++ test/uai2014.jl
@@ -5,59 +5,91 @@ using JunctionTrees
 using JunctionTrees: read_uai_file, read_uai_evid_file, read_uai_mar_file
 
 benchmarks = [
-              # "Alchemy",
-              # "CSP",
-              # "DBN",
-              # "Grids",
+              "Alchemy",
+              "CSP",
+              "DBN",
+              "Grids",
               # "linkage", # fails: OutOfMemoryError (problems 15,20,23)
               # "ObjectDetection", # fails from 36 onwards
-              # "Pedigree",
+              "Pedigree",
               "Promedus",
               # "relational", # fails: OutOfMemoryError (problem 1), Kill signal (problem 2)
-              # "Segmentation",
+              "Segmentation",
              ]
 
-@testset "Default" begin
-  for benchmark in benchmarks
+for benchmark in benchmarks
 
-    @testset "$(benchmark) benchmark" begin
+  @testset "$(benchmark) benchmark" begin
 
-      println("Benchmark: $(benchmark)")
+    println("Benchmark: $(benchmark)")
 
-      rexp = Regex("($(benchmark)_\\d*)(\\.uai)\$") 
-      problems = readdir(artifact"uai2014"; sort=false) |> 
-        x -> map(y -> match(rexp, y), x) |>
-        x -> filter(!isnothing, x) |>
-        x -> map(first, x)
+    rexp = Regex("($(benchmark)_\\d*)(\\.uai)\$") 
+    problems = readdir(artifact"uai2014"; sort=false) |> 
+      x -> map(y -> match(rexp, y), x) |>
+      x -> filter(!isnothing, x) |>
+      x -> map(first, x)
 
-      for problem in problems
+    for problem in problems
 
-        @testset "$(problem)" begin
+      @testset "$(problem)" begin
 
-          println("  Problem: $(problem)")
+        println("  Problem: $(problem)")
 
-          uai_filepath = joinpath(artifact"uai2014", problem * ".uai")
-          uai_evid_filepath = joinpath(artifact"uai2014", problem * ".uai.evid")
-          uai_mar_filepath = joinpath(artifact"uai2014", problem * ".uai.MAR")
-          td_filepath = joinpath(artifact"uai2014", problem * ".tamaki.td")
+        uai_filepath = joinpath(artifact"uai2014", problem * ".uai")
+        uai_evid_filepath = joinpath(artifact"uai2014", problem * ".uai.evid")
+        uai_mar_filepath = joinpath(artifact"uai2014", problem * ".uai.MAR")
+        td_filepath = joinpath(artifact"uai2014", problem * ".tamaki.td")
 
-          nvars, cards, nclique, factors = read_uai_file(uai_filepath; factor_eltype=Float64)
-          obsvars, obsvals = read_uai_evid_file(uai_evid_filepath)
-          reference_marginals = read_uai_mar_file(uai_mar_filepath)
+        nvars, cards, nclique, factors = read_uai_file(uai_filepath; factor_eltype=Float64)
+        obsvars, obsvals = read_uai_evid_file(uai_evid_filepath)
+        reference_marginals = read_uai_mar_file(uai_mar_filepath)
 
-          algo = compile_algo(
-                              uai_filepath;
-                              uai_evid_filepath = uai_evid_filepath, 
-                              td_filepath = td_filepath,
-                              factor_eltype=Float64,
-                            )
+        # ------------------------------------------------------------------------------
+        # Default
+        # ------------------------------------------------------------------------------
 
-          eval(algo)
-          marginals = run_algo(obsvars, obsvals) |> x -> map(y -> y.vals, x)
+        println("    Test type: default")
 
-          @test isapprox(marginals, reference_marginals, atol=0.01)
+        println("      Compiling algo...")
 
-        end
+        algo = compile_algo(
+                            uai_filepath;
+                            uai_evid_filepath = uai_evid_filepath, 
+                            td_filepath = td_filepath,
+                            factor_eltype=Float64,
+                          )
+
+        eval(algo)
+
+        println("      Running algo...")
+        marginals = run_algo(obsvars, obsvals) |> x -> map(y -> y.vals, x)
+
+        @test isapprox(marginals, reference_marginals, atol=0.01)
+
+        # ------------------------------------------------------------------------------
+        # OMEinsum backend
+        # ------------------------------------------------------------------------------
+
+        println("    Test type: OMEinsum backend")
+
+        println("      Compiling algo...")
+
+        algo = compile_algo(
+                            uai_filepath;
+                            uai_evid_filepath = uai_evid_filepath, 
+                            td_filepath = td_filepath,
+                            factor_eltype=Float64,
+                          )
+
+        algo = boost_algo(algo)
+
+        eval(algo)
+
+        println("      Running algo...")
+        marginals = run_algo(obsvars, obsvals) |> x -> map(y -> y.vals, x)
+
+        @test isapprox(marginals, reference_marginals, atol=0.01)
+
       end
     end
   end
