5eee387b0c44e3ee8eab25204929ba814ad41655
Martin Roa Villescas <mroavi@gmail.com>
2022-10-21 17:19:57 +0200

    Move the UAI2014 benchmark tests outside this package

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,12 +19,6 @@ end
   include("junction_tree_algorithm.jl")
 end
 
-@testset "uai2014" begin
-  include("uai2014/partial_evaluation.jl")
-  include("uai2014/fp_overflow_correction.jl")
-  include("uai2014/omeinsum.jl")
-end
-
 @testset "doctests" begin
   include("doctests.jl")
 end
